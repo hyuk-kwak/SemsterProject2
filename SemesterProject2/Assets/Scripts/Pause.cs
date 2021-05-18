69d87c33a8fc3e921a8b38cbd2b56272bc88b69b
jihun0916 <69163541+jihun0916@users.noreply.github.com>
2021-05-18 17:03:32 +0900

    pause UI

---
+++ SemesterProject2/Assets/Scripts/Pause.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    bool IsPaused;
+    float TimeCheck;
+    public float delayTime;
+    
+    public Button ListBtn;
+    public Button PlayBtn;
+    public Button RestartBtn;
+    public AudioMixer audioMixer;
+    public Slider SoundSlider;
+
+
+    public void Awake()
+    {
+        IsPaused = true;
+        TimeCheck = 0;
+    }
+    
+        
+    
+    public void AudioControl() //슬라이더로 소리 볼륨 조절
+    {
+        float sound = SoundSlider.value;
+
+        if (sound == -40f) audioMixer.SetFloat("BGM", -80); //음소거시
+        else audioMixer.SetFloat("BGM", sound); 
+    }
+
+    public void listBtnOnClick()
+    {
+        SceneManager.LoadScene("");//게임 로비화면 scene 호출
+    }
+    public void playBtnOnClick()
+    {
+        Destroy(gameObject);//pause UI 삭제
+
+        TimeCheck += Time.deltaTime; //계속 플레이하기 전 대기시간
+        if(TimeCheck == delayTime)
+        {
+            //화면에 카운트 띄우기
+            IsPaused = false;
+        }
+    }
+
+    public void restartBtnOnClick()
+    {
+        SceneManager.LoadScene(""); //현재 게임 scene 호출
+    }
+
+
+
+
+
+
+}
