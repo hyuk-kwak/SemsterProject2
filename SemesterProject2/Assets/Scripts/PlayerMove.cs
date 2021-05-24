aa7a3ea1b74dd7760190db0c7387f86c64fa605d
hyuk-kwak <starcaft99@nate.com>
2021-05-24 18:10:01 +0900

    과연 커밋 될것인가 ....

--- SemesterProject2/Assets/Scripts/PlayerMove.cs
+++ SemesterProject2/Assets/Scripts/PlayerMove.cs
@@ -7,13 +7,22 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public GameObject Player;
-    
+    Animator PlayerMotion;
+    bool isWalk = true;
+    bool isAttack = false;
+    bool isSkill_1 = false;
+    bool isSkill_2 = false;
 
     float Dir = 1.0f;
-    float Speed = 0.1f;
+    float Speed = 0.05f;
     public bool jump = false;
-   
 
+    float Timer = 0.0f;
+
+    private void Awake()
+    {
+        PlayerMotion = Player.GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -39,12 +48,44 @@ public class PlayerMove : MonoBehaviour
             Player.GetComponent<Rigidbody2D>().gravityScale = -1;
             jump = false;
         }
-
+        
         if (Player.transform.position.y > 2.7f)
         {
             Player.GetComponent<Rigidbody2D>().gravityScale = 1;
         }
-
+        // 스킬
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("공격");
+                isAttack = true;
+                PlayerMotion.SetInteger("motion", 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                isSkill_1 = true;
+                PlayerMotion.SetInteger("motion", 2);
+                Debug.Log("스킬1");
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                isSkill_2 = true;
+                PlayerMotion.SetInteger("motion", 3);
+                Debug.Log("스킬2");
+            }
+        }
+        if(isAttack || isSkill_1 || isSkill_2)
+        {
+            Timer += Time.deltaTime;
+            if(Timer > 0.4f)
+            {
+                PlayerMotion.SetInteger("motion", 0);
+                isAttack = false;
+                isSkill_1 = false;
+                isSkill_2 = false;
+                Timer = 0.0f;
+            }
+        }
 
         Player.transform.position += new Vector3(Dir, 0, 0) * Speed;
         
--- SemesterProject2/Assets/Scripts/PlayerSkill.cs
+++ SemesterProject2/Assets/Scripts/PlayerSkill.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSkill
+public class PlayerSkill : MonoBehaviour
 {
+    public GameObject Player;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+    //void OnCollisionEnter(Collision Enemy)
+    //{
+    //    if (Enemy.collider.tag == "mongkey" || Enemy.collider.tag == "kangeroo")
+    //    {
+    //        if (isAttack || isSkill_1 || isSkill_2)
+    //        {
+
+    //        }
+           
+    //    }
+    //}
 }
